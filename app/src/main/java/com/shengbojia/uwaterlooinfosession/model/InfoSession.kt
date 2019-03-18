c147ae5bbf5e4d46a5d3568db55ccc29c1384a33
Shengbo Jia <shengbojia@gmail.com>
2019-03-18 17:42:48 -0400

    Initial commit

---
+++ app/src/main/java/com/shengbojia/uwaterlooinfosession/model/InfoSession.kt
@@ -0,0 +1,26 @@
+package com.shengbojia.uwaterlooinfosession.model
+
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+data class InfoSession(
+    @PrimaryKey
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("employer")
+    val employer: String,
+    @SerializedName("date")
+    val date: String,
+    @SerializedName("day")
+    val day: String,
+    @SerializedName("start_time")
+    val startTime: String,
+    @SerializedName("end_time")
+    val endTime: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("website")
+    val website: String
+
+) {
+}
