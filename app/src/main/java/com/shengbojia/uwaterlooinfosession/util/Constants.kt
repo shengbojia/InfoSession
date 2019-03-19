7eccc49d0a67b173c017d1646a4a8363ef0bb746
Shengbo Jia <shengbojia@gmail.com>
2019-03-18 23:03:33 -0400

    Added BASE_URL constant

--- app/src/main/java/com/shengbojia/uwaterlooinfosession/util/Constants.kt
+++ app/src/main/java/com/shengbojia/uwaterlooinfosession/util/Constants.kt
@@ -4,3 +4,4 @@ package com.shengbojia.uwaterlooinfosession.util
  * Constants used throughout the app.
  */
 const val API_KEY = "2449271ba237e09ca3eb2ca7bce47347"
+const val BASE_URL = "https://api.uwaterloo.ca/v2/"
